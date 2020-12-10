7a22f88014b207ea5779f638ea1090ebc9c11eed
Tyler Langlois <tjl@byu.net>
2020-12-10 02:36:31 -0700

    Support native builds

--- project.clj
+++ project.clj
@@ -1,11 +1,17 @@
-(defproject aoc-clj "0.1.0-SNAPSHOT"
+(defproject aoc-clj "0.1.0"
   :description "Advent of Code solutions in Clojure"
   :url "https://github.com/tylerjl/aoc-clj"
   :license {:name "GPL3"
             :url "https://choosealicense.com/licenses/gpl-3.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.10.2-alpha4"]
                  [org.clojure/math.combinatorics "0.1.6"]
                  [org.clojure/tools.cli "1.0.194"]]
   :repl-options {:init-ns aoc-clj.core}
   :main aoc-clj.core
+  :plugins [[io.taylorwood/lein-native-image "0.3.1"]]
+  :target-path "target/%s"
+  :native-image {:jvm-opts ["-Dclojure.compiler.direct-linking=true"]
+                 :opts ["--report-unsupported-elements-at-runtime"
+                        "--initialize-at-build-time"]
+                 :name "aoc-clj"}
   :aot [aoc-clj.core])
